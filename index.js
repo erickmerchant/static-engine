14a605216df7519a2d9619b6e179fc7cfecb1bfa
Erick Merchant <erickmerchant@gmail.com>
2014-12-06 19:13:42 -0800

    Completely change the api
    Rendering will be handled by a plugin now. The module is now a single function that accepts an array of arrays of plugins. It returns a promise.

--- index.js
+++ index.js
@@ -1,72 +1,18 @@
-var path = require('path');
-var fs = require('fs');
 var Promise = require('es6-promise').Promise;
-var mkdirp = require('mkdirp');
-var interpolate = require('./interpolate.js');
 
-function Site(site_directory, renderer) {
+module.exports = function (formulas) {
 
-    this.site_directory = site_directory;
+    var promises = formulas.map(function(plugins) {
 
-    this.renderer = renderer;
-
-    this.routes = [];
-
-    this.befores = [];
-
-    this.afters = [];
-}
-
-Site.prototype = {
-
-    route: function (route) {
-
-        return new Route(this, route);
-    },
-
-    build: function () {
-
-        var site = this;
-
-        var route_promises = site.routes.map(function (route){
-
-            return site._run(route).then(function(pages){
-
-                var render_promises = [];
-
-                if (route.template) {
-
-                    if (!pages.length) {
-
-                        pages = [{}];
-                    }
-
-                    render_promises = pages.map(function (page) {
-
-                        return site._render(route, page);
-                    });
-                }
-
-                return Promise.all(render_promises);
-            });
-        });
-
-        return Promise.all(route_promises);
-    },
-
-    _run: function(route) {
-
-        var site = this;
+        var i = -1;
 
         return new Promise(function(resolve, reject){
 
-            var i = -1;
-
             var next = function(pages) {
 
-                if (++i < route.plugins.length) {
+                if (++i < plugins.length) {
 
-                    route.plugins[i](pages, next);
+                    plugins[i](pages).then(next);
 
                     return;
                 }
@@ -74,93 +20,9 @@ Site.prototype = {
                 resolve(pages);
             };
 
-            Array.prototype.unshift.apply(route.plugins, site.befores);
-
-            Array.prototype.push.apply(route.plugins, site.afters);
-
             next([]);
         });
-    },
+    });
 
-    _render: function(route, page) {
-
-        var site = this;
-
-        return new Promise(function(resolve, reject){
-
-            if(site.renderer) {
-
-                site.renderer(route.template, page, function (err, html) {
-
-                    var url = interpolate(route.route, page || {});
-
-                    var file = site.site_directory + url;
-
-                    var directory;
-
-                    directory = path.dirname(file);
-
-                    mkdirp(directory, function (err) {
-
-                        if (err) reject(err);
-
-                        fs.writeFile(file, html, function (err, data) {
-
-                            if (err) reject(err);
-
-                            resolve();
-                        });
-                    });
-                });
-            }
-        });
-    },
-
-    before: function (before) {
-
-        this.befores.push(before);
-    },
-
-    after: function (after) {
-
-        this.afters.push(after);
-    }
-};
-
-function Route(site, route) {
-
-    this.site = site;
-
-    this.route = route;
-
-    this.plugins = [];
-}
-
-Route.prototype = {
-
-    use: function (use) {
-
-        if (typeof use == 'function') {
-
-            this.plugins.push(use);
-
-            return this;
-        }
-
-        throw new Error("'use' only accepts a function");
-    },
-
-    render: function (template) {
-
-        this.template = template;
-
-        this.site.routes.push(this);
-
-        return this;
-    }
-};
-
-module.exports = function (site_directory, renderer) {
-
-    return new Site(site_directory, renderer);
+    return Promise.all(promises);
 };
--- test/test.js
+++ test/test.js
@@ -1,359 +1,109 @@
 var assert = require('chai').assert;
 var engine = require('../index.js');
-var mock = require('mock-fs');
-var fs = require('fs');
-var site_directory = './test/build/';
+var Promise = require('es6-promise').Promise;
 
-function push(literal) {
+describe('engine', function(){
 
-    return function (pages, next) {
+    it('accept an array of arrays of plugins and return a promise', function(done) {
 
-        pages.push(literal);
+        var promise = engine([
+            [
+                function(pages){
+                    pages.push('a');
+                    return Promise.resolve(pages);
+                },
+                function(pages){
+                    pages.push('b');
+                    return Promise.resolve(pages);
+                },
+                function(pages){
+                    pages.push('c');
+                    return Promise.resolve(pages);
+                }
+            ],
+            [
+                function(pages){
+                    pages.push('d');
+                    return Promise.resolve(pages);
+                },
+                function(pages){
+                    pages.push('e');
+                    return Promise.resolve(pages);
+                },
+                function(pages){
+                    pages.push('f');
+                    return Promise.resolve(pages);
+                }
+            ],
+            [
+                function(pages){
+                    pages.push('g');
+                    return Promise.resolve(pages);
+                },
+                function(pages){
+                    pages.push('h');
+                    return Promise.resolve(pages);
+                },
+                function(pages){
+                    pages.push('i');
+                    return Promise.resolve(pages);
+                }
+            ]
+        ]);
 
-        next(pages);
-    };
-};
-
-function render(template, page, done) {
-
-    done(null, (page && page.test ? page.test : ''));
-};
-
-beforeEach(function() { mock({'./test/build/': {}})  });
-
-describe('site', function () {
-
-    describe('.constructor()', function(){
-
-        it('instance should have expected methods', function (done) {
-
-            var site = engine(site_directory, render);
-
-            assert.isFunction(site.route);
-
-            assert.isFunction(site.build);
-
-            assert.isFunction(site.before);
-
-            assert.isFunction(site.after);
-
-            done();
-        });
+        promise.then(function(){ done(); });
     });
 
-    describe('.before()', function(){
+    it('plugins should have the desired effect', function(done) {
 
-        it('should add plugin that\'s called before', function (done) {
-
-            var site = engine(site_directory, render);
-
-            site.route('test.html').render('test.html');
-
-            site.before(function(pages, next){
-
-                pages.push({test: 'test-a'})
-
-                next(pages);
-            });
-
-            site.build().then(
-                function(){
-
-                    fs.readFile(site_directory+'test.html', function (err, data) {
-
-                        if (err) done(err);
-                        else {
-
-                            assert.equal(data, 'test-a');
-
-                            done();
-                        }
-                    });
+        var promise = engine([
+            [
+                function(pages){
+                    pages.push('a');
+                    return Promise.resolve(pages);
                 },
-                function(err) {
-
-                    done(err);
-                }
-            );
-        });
-    });
-
-    describe('.after()', function(){
-
-        it('should add plugin that\'s called after', function (done) {
-
-            var site = engine(site_directory, render);
-
-            site.route('test.html').use(push({test:'test-a'})).render('test.html');
-
-            site.after(function(pages, next){
-
-                pages.forEach(function(page, i){
-
-                    pages[i].test = 'test-b';
-                });
-
-                next(pages);
-            });
-
-            site.build().then(
-                function(){
-
-                    fs.readFile(site_directory+'test.html', function (err, data) {
-
-                        if (err) done(err);
-                        else {
-
-                            assert.equal(data, 'test-b');
-
-                            done();
-                        }
-                    });
+                function(pages){
+                    pages.push('b');
+                    return Promise.resolve(pages);
                 },
-                function(err) {
-
-                    done(err);
+                function(pages){
+                    pages.push('c');
+                    return Promise.resolve(pages);
                 }
-            );
-        });
-    });
-
-    describe('.build()', function () {
-
-        it('site_directory should contain the expected files when pages.length == 0', function (done) {
-
-            var site = engine(site_directory, render);
-
-            site.route('test.html').render('test.html');
-
-            site.build().then(
-                function(){
-
-                    fs.readdir(site_directory, function (err, files) {
-
-                        if (err) done(err);
-                        else {
-
-                            assert.lengthOf(files, 1);
-
-                            assert.include(files, 'test.html');
-
-                            done();
-                        }
-                    });
+            ],
+            [
+                function(pages){
+                    pages.push('d');
+                    return Promise.resolve(pages);
                 },
-                function(err) {
-
-                    done(err);
-                }
-            );
-        });
-
-        it('site_directory should contain the expected files when pages.length > 1', function (done) {
-
-            var site = engine(site_directory, render);
-
-            site.route('{test}.html').use(push({test: 'test-a'})).use(push({test: 'test-b'})).render('test.html');
-
-            site.build().then(
-                function(){
-
-                    fs.readdir(site_directory, function (err, files) {
-
-                        if (err) done(err);
-                        else {
-
-                            assert.lengthOf(files, 2);
-
-                            assert.include(files, 'test-a.html');
-
-                            assert.include(files, 'test-b.html');
-
-                            done();
-                        }
-                    });
+                function(pages){
+                    pages.push('e');
+                    return Promise.resolve(pages);
                 },
-                function(err) {
-
-                    done(err);
+                function(pages){
+                    pages.push('f');
+                    return Promise.resolve(pages);
                 }
-            );
-        });
-
-        it('plugin should be called in the correct order', function (done) {
-
-            var site = engine(site_directory, render);
-
-            var order = 0;
-
-            var push = function(literal) {
-
-                return function (pages, next) {
-
-                    literal.order = order++;
-
-                    pages.push(literal);
-
-                    next(pages);
-                };
-            };
-site.route('{test}-{order}.html').use(push({test:'test-c'})).use(push({test:'test-d'})).render('test.html');
-
-            site.before(push({test:'test-a'}));
-
-            site.before(push({test:'test-b'}));
-
-            site.after(push({test:'test-e'}));
-
-            site.after(push({test:'test-f'}));
-
-            site.build().then(
-                function(){
-
-                    fs.readdir(site_directory, function (err, files) {
-
-                        if (err) done(err);
-                        else {
-
-                            assert.include(files, 'test-a-0.html');
-
-                            assert.include(files, 'test-b-1.html');
-
-                            assert.include(files, 'test-c-2.html');
-
-                            assert.include(files, 'test-d-3.html');
-
-                            assert.include(files, 'test-e-4.html');
-
-                            assert.include(files, 'test-f-5.html');
-
-                            done();
-                        }
-                    });
+            ],
+            [
+                function(pages){
+                    pages.push('g');
+                    return Promise.resolve(pages);
                 },
-                function(err) {
-
-                    done(err);
-                }
-            );
-        });
-
-        it('interpolate should throw an error when a var isn\'t defined', function (done) {
-
-            var site = engine(site_directory, render);
-
-            site.route('{test}.html').render('test.html');
-
-            site.build().then(
-                function(){
-
-                    assert.notOk(true);
-
-                    done();
+                function(pages){
+                    pages.push('h');
+                    return Promise.resolve(pages);
                 },
-                function(err){
-
-                    assert.ok(true);
-
-                    done();
+                function(pages){
+                    pages.push('i');
+                    return Promise.resolve(pages);
                 }
-            );
-        });
+            ]
+        ]);
 
+        promise.then(function(pages){
 
-        it('each file should contain what is expected', function (done) {
-
-            var site = engine(site_directory, render);
-
-            site.route('test.html').use(push({test: 'test-a'})).render('test.html');
-
-            site.build().then(
-                function(){
-
-                    fs.readFile(site_directory+'test.html', function (err, data) {
-
-                        if(err) done(err);
-                        {
-                            assert.equal(data, 'test-a');
-
-                            done();
-                        }
-                    });
-                },
-                function(err) {
-
-                    done(err);
-                }
-            );
-        });
-    });
-
-    describe('.route()', function(){
-
-        it('it should create and return a route', function (done) {
-
-            var site = engine(site_directory, render);
-
-            var route = site.route('test.html');
-
-            assert.isFunction(route.use);
-
-            assert.isFunction(route.render);
-
-            done();
-        });
-    });
-});
-
-describe('route', function(){
-
-    describe('.use()', function(){
-
-        it('should throw an error if a function is not given', function(done) {
-
-            var site = engine(site_directory, render);
-
-            var route = site.route('test.html');
-
-            try
-            {
-                route.use('test');
-
-                assert.notOk(true);
-            }
-            catch(e)
-            {
-                assert.ok(true);
-            }
-
-            done();
-        });
-
-        it('should return the route', function(done) {
-
-            var site = engine(site_directory, render);
-
-            var route = site.route('test.html');
-
-            var result = route.use(function(){});
-
-            assert.equal(route, result);
-
-            done();
-        });
-    });
-
-    describe('.render()', function(){
-
-        it('should return the route', function(done) {
-
-            var site = engine(site_directory, render);
-
-            var route = site.route('test.html');
-
-            var result = route.render('test.html');
-
-            assert.equal(route, result);
+            assert.deepEqual(pages, [['a','b','c'],['d','e','f'],['g','h','i']]);
 
             done();
         });
