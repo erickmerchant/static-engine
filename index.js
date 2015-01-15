730e1d711e4d55289cc4a6afa18c0dbd80525cb6
Erick Merchant <erickmerchant@gmail.com>
2015-01-14 22:50:22 -0800

    Allow for a single array or multiple arguments

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var compose = require('static-compose');
 
 module.exports = function (formulas) {
 
+    if(!Array.isArray(formulas) || arguments.length > 1) {
+
+        formulas = [].slice.call(arguments);
+    }
+
     var promises = formulas.map(function(plugins) {
 
         return compose(plugins)([]);
