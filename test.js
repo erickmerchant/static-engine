49f71261a03a0afd7786370e82c99607f8f7afaf
Erick Merchant <erickmerchant@gmail.com>
2015-10-25 02:06:15 -0700

    Switch tests to tap

--- test.js
+++ test.js
@@ -1,92 +1,88 @@
-var assert = require('assert')
 var engine = require('./index.js')
-var describe = require('mocha').describe
-var it = require('mocha').it
+var tap = require('tap')
 
-describe('engine', function () {
-  var promise = engine(
-    [
-      function (pages, done) {
-        pages.push('a')
-        done(null, pages)
-      },
-      function (pages, done) {
-        pages.push('b')
-        done(null, pages)
-      }
-    ],
-    [
-      function (pages) {
-        return new Promise(function (resolve, reject) {
-          pages.push('c')
-          resolve(pages)
-        })
-      },
-      function (pages) {
-        return new Promise(function (resolve, reject) {
-          pages.push('d')
-          resolve(pages)
-        })
-      }
-    ]
-  )
+var promise = engine(
+  [
+    function (pages, done) {
+      pages.push('a')
+      done(null, pages)
+    },
+    function (pages, done) {
+      pages.push('b')
+      done(null, pages)
+    }
+  ],
+  [
+    function (pages) {
+      return new Promise(function (resolve, reject) {
+        pages.push('c')
+        resolve(pages)
+      })
+    },
+    function (pages) {
+      return new Promise(function (resolve, reject) {
+        pages.push('d')
+        resolve(pages)
+      })
+    }
+  ]
+)
 
-  it('should return a promise', function (done) {
-    assert.ok(promise instanceof Promise)
+tap.test('should return a promise', function (t) {
+  t.ok(promise instanceof Promise)
 
-    done()
+  t.end()
+})
+
+tap.test('should have expected results', function (t) {
+  promise.then(function (pages) {
+    t.deepEqual(pages, [['a', 'b'], ['c', 'd']])
+
+    t.end()
   })
+  .catch(t.end)
+})
 
-  it('should have expected results', function (done) {
-    promise.then(function (pages) {
-      assert.deepEqual(pages, [['a', 'b'], ['c', 'd']])
+var failingFromCallback = engine(
+  [
+    function (pages, done) {
+      pages.push('a')
+      done(null, pages)
+    },
+    function (pages, done) {
+      done(new Error('An error occurred'))
+    }
+  ]
+)
 
-      done()
-    })
-    .catch(done)
+tap.test('should handle errors from callbacks', function (t) {
+  failingFromCallback.catch(function (err) {
+    t.equal('An error occurred', err.message)
+
+    t.end()
   })
+})
 
-  var failingFromCallback = engine(
-    [
-      function (pages, done) {
-        pages.push('a')
-        done(null, pages)
-      },
-      function (pages, done) {
-        done(new Error('An error occurred'))
-      }
-    ]
-  )
+var failingFromPromise = engine(
+  [
+    function (pages) {
+      return new Promise(function (resolve, reject) {
+        pages.push('c')
+        resolve(pages)
+      })
+    },
+    function (pages) {
+      return new Promise(function (resolve, reject) {
+        reject(new Error('An error occurred'))
+      })
+    }
+  ]
+)
 
-  it('should handle errors from callbacks', function (done) {
-    failingFromCallback.catch(function (err) {
-      assert.equal('An error occurred', err.message)
+tap.test('should handle errors from promises', function (t) {
+  failingFromPromise.catch(function (err) {
+    t.equal('An error occurred', err.message)
 
-      done()
-    })
-  })
-
-  var failingFromPromise = engine(
-    [
-      function (pages) {
-        return new Promise(function (resolve, reject) {
-          pages.push('c')
-          resolve(pages)
-        })
-      },
-      function (pages) {
-        return new Promise(function (resolve, reject) {
-          reject(new Error('An error occurred'))
-        })
-      }
-    ]
-  )
-
-  it('should handle errors from promises', function (done) {
-    failingFromPromise.catch(function (err) {
-      assert.equal('An error occurred', err.message)
-
-      done()
-    })
+    t.end()
   })
 })
